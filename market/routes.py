099b31baa75c203cfa15322b135307abc3052f48
Talion90 <76944100+Talion90@users.noreply.github.com>
2021-09-13 12:52:38 +0300

    Add Flash messages and validation methods

--- market/routes.py
+++ market/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, flash
 
 from market import app
 from market.models import Item, User
@@ -30,5 +30,5 @@ def register_page():
         return redirect(url_for('market_page'))
     if form.errors != {}:  # If there are not errors from the validations
         for err_msg in form.errors.values():
-            print(f'There was an error with creating a user: {err_msg}')
+            flash(f'There was an error with creating a user: {err_msg}', category='danger')
     return render_template('register.html', form=form)
